713c3e01067061daac70ef4c23ed8a8386533707
Code JSON <73917308+codragon2020@users.noreply.github.com>
2022-03-02 06:24:22 -0500

    Adding props to Counter and prop-type validation

--- frontend/todo-app/src/components/counter/Counter.jsx
+++ frontend/todo-app/src/components/counter/Counter.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import './Counter.css';
 
 class Counter extends Component {
@@ -16,7 +17,7 @@ class Counter extends Component {
 	render() {
 		return (
 			<div className="firstComponent">
-				<button onClick={this.increment}>+1</button>
+				<button onClick={this.increment}>+{this.props.by}</button>
 				<span className="count">{this.state.counter}</span>
 			</div>
 		);
@@ -25,11 +26,18 @@ class Counter extends Component {
     increment() {
         //console.log('increment');
         this.setState({
-            counter: this.state.counter + 1
+            counter: this.state.counter + this.props.by
         })
     }
 }
 
+Counter.defaultProps = {
+    by : 1
+}
 
+//Ensure that the Counter prop will be a number
+Counter.propTypes = {
+    by : PropTypes.number
+}
 
 export default Counter;
