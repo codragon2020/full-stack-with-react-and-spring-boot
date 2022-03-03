16e2abb9ff8a4f77254956884af6716f74eb69f9
Code JSON <73917308+codragon2020@users.noreply.github.com>
2022-03-03 06:33:02 -0500

    Creating Counter class, bringing State and increment into the Counter parent

--- frontend/todo-app/src/App.js
+++ frontend/todo-app/src/App.js
@@ -5,10 +5,8 @@ import './App.css';
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <Counter by={1}/>
-        <Counter by={5}/>
-        <Counter by={10}/>
+      <div className="counter">
+        <Counter/>
       </div>
     );
   }
--- frontend/todo-app/src/components/counter/Counter.jsx
+++ frontend/todo-app/src/components/counter/Counter.jsx
@@ -3,41 +3,67 @@ import PropTypes from 'prop-types';
 import './Counter.css';
 
 class Counter extends Component {
-
-    //Define the initial state in a constructor
-    constructor() {
-        super();
-        this.state = {
-            counter : 0
-        }
-
-        this.increment = this.increment.bind(this);
-    }
+	constructor() {
+		super();
+		this.state = {
+			counter: 0,
+		};
+		this.increment = this.increment.bind(this);
+	}
 
 	render() {
 		return (
-			<div className="firstComponent">
-				<button onClick={this.increment}>+{this.props.by}</button>
+			<div className="counter">
+				<CounterButton by={1} incrementMethod={this.increment} />
+				<CounterButton by={5} incrementMethod={this.increment} />
+				<CounterButton by={10} incrementMethod={this.increment} />
 				<span className="count">{this.state.counter}</span>
 			</div>
 		);
 	}
 
-    increment() {
-        //console.log('increment');
-        this.setState({
-            counter: this.state.counter + this.props.by
-        })
-    }
+	increment(by) {
+		//console.log(`increment from parent - ${by}`)
+		this.setState((prevState) => {
+			return { counter: prevState.counter + by };
+		});
+	}
 }
 
-Counter.defaultProps = {
-    by : 1
+class CounterButton extends Component {
+	//Define the initial state in a constructor
+	constructor() {
+		super();
+		this.state = {
+			counter: 0,
+		};
+
+		this.increment = this.increment.bind(this);
+	}
+
+	render() {
+		return (
+			<div className="counter">
+				<button onClick={this.increment}>+{this.props.by}</button>
+			</div>
+		);
+	}
+
+	increment() {
+		this.setState({
+			counter: this.state.counter + this.props.by,
+		});
+		this.props.incrementMethod(this.props.by);
+	}
 }
 
-//Ensure that the Counter prop will be a number
-Counter.propTypes = {
-    by : PropTypes.number
-}
+CounterButton.defaultProps = {
+	by: 1,
+};
+
+//Ensure that the CounterButton prop will be a number
+CounterButton.propTypes = {
+	by: PropTypes.number,
+};
 
 export default Counter;
