d5d6b56078fa5837134b7eec435e2d657efaba20
Code JSON <73917308+codragon2020@users.noreply.github.com>
2022-03-05 10:00:58 -0500

    Passing values from one component to another using withParams

--- frontend/todo-app/src/components/todo/TodoApp.jsx
+++ frontend/todo-app/src/components/todo/TodoApp.jsx
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
 import {BrowserRouter as Router, Route, Routes} from 'react-router-dom'
 import withNavigation from './WithNavigation';
+import withParams from './withParams';
 
 class TodoApp extends Component {
 	render() {
         const LoginComponentWithNavigation = withNavigation(LoginComponent);
+        const WelcomeComponentWithParams = withParams(WelcomeComponent);
 		return (
 			<div className="TodoApp">
                 <Router>
@@ -12,7 +14,8 @@ class TodoApp extends Component {
                         <Route path="/" element={<LoginComponent />} />
                         <Route path="/login" element={<LoginComponentWithNavigation />}/>
                         {/* <Route path="/login" element={<LoginComponent />}/> */}
-                        <Route path="/welcome" element={<WelcomeComponent />}/>
+                        {/* <Route path="/welcome/:name" element={<WelcomeComponent />}/> */}
+                        <Route path="/welcome/:name" element={<WelcomeComponentWithParams />} />
                         <Route path="*" element={<ErrorComponent/>}/>
                     </Routes>
                 </Router>
@@ -23,7 +26,7 @@ class TodoApp extends Component {
 
 class WelcomeComponent extends Component {
     render() {
-        return <div>Welcome to my Website</div>
+        return <div>Welcome to my Website {this.props.params.name}</div>
     }
 }
 
@@ -53,7 +56,7 @@ class LoginComponent extends Component {
 
     loginClicked() {
         if(this.state.username==='IndiaPaleAle' && this.state.password==='Test123$') {
-            this.props.navigate("/welcome")
+            this.props.navigate(`/welcome/${this.state.username}`)
             // this.setState({showSuccessMessage:true})
             // this.setState({hasLoginFailed:false})
             
